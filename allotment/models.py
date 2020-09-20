428ae7453355566fcba167cf641c68bd6ebd83a7
Rahul Patel <rpmit1061@gmail.com>
2020-09-20 12:29:43 +0530

    new changes

--- allotment/models.py
+++ allotment/models.py
@@ -35,6 +35,8 @@ class UploadFile(models.Model):
 
 class RoomCreate(models.Model):
 #     room_no = models.IntegerField()
-#     no_col = models.IntegerField()
+#     no_col = models.IntegerField()git 
+
+
     no_row = models.IntegerField()
     total_capacity = models.IntegerField()
